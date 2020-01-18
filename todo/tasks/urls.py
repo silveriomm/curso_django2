b04871e75024f2f3c37d07e5da2009eb649ccd33
silveriomm <silveriomm@bol.com.br>
2020-01-18 18:29:23 -0200

    criando views e templates-aula04

--- todo/tasks/urls.py
+++ todo/tasks/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('helloworld/', views.helloworld),
+    path('', views.taskList, name='taskList'),
+    path('yourname/<str:name>', views.yourName, name='your-name'),
 ]
