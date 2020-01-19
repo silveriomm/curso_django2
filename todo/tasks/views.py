ba2da372a8b0facb5310ee112e8ef932d7af855c
silveriomm <silveriomm@bol.com.br>
2020-01-19 00:27:21 -0200

    inserindo dados pelo form-aula10

--- todo/tasks/views.py
+++ todo/tasks/views.py
@@ -1,17 +1,24 @@
-from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404, redirect
+from .forms import TaskForm
 from .models import Task
 
-def helloworld(request):
-    return HttpResponse('Hello, World!')
-
 def taskList(request):
-    tasks = Task.objects.all()
+    tasks = Task.objects.all().order_by('-create_at')
     return render(request, 'tasks/list.html',{'tasks':tasks})
 
-def yourName(request, name):
-    return render(request, 'tasks/yourname.html', {'name':name})
+def newTask(request):
+    if request.method == 'POST':
+        form = TaskForm(request.POST)
+        if form.is_valid():
+            task = form.save(commit=False)
+            task.done = 'doing'
+            task.save()
+            return redirect('/')
+    else:
+        form = TaskForm()
+        return render(request, 'tasks/addtask.html', {'form':form})
 
 def taskView(request, id):
     task = get_object_or_404(Task, pk=id)
     return render(request, 'tasks/task.html', {'task':task})
+
