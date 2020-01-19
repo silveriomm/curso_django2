8ff6207b2e5825472032b38d5bd2f260b25fbe26
silveriomm <silveriomm@bol.com.br>
2020-01-19 08:06:06 -0200

    editando tarefa=aula11

--- todo/tasks/urls.py
+++ todo/tasks/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('', views.taskList, name='task-list'),
     path('task/<int:id>', views.taskView, name='task-view'),
     path('newtask/', views.newTask, name='new-task'),
+    path('edit/<int:id>', views.editTask, name='edit-task'),
 ]
