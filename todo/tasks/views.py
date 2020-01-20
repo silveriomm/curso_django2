3979d14405d50faa77901bae042aab91292a8261
silveriomm <silveriomm@bol.com.br>
2020-01-19 22:14:13 -0200

    adicionado concluir tarefa - aula 18

--- todo/tasks/views.py
+++ todo/tasks/views.py
@@ -59,6 +59,19 @@ def deleteTask(request, id):
     messages.info(request, 'Tarefa excluída com sucesso!')
     return redirect('/')
 
+
+@login_required
+def changeStatus(request, id):
+    task = get_object_or_404(Task, pk=id)
+    if(task.done == 'doing'):
+        task.done = 'done'
+    else:
+        task.done = 'doing'
+
+    task.save()
+    return redirect('/')
+
+
 @login_required
 def taskView(request, id):
     task = get_object_or_404(Task, pk=id)
